2f4d116b50cbdc9819f870f94596fb7673e304f1
mohamadzarei45 <75812599+mohamadzarei45@users.noreply.github.com>
2020-12-11 01:30:50 +0330

    Create 0000-01-02-mohamadzarei45.cpp
    I'm learning c++ and I progressed this much , any help?

---
+++ _posts/0000-01-02-mohamadzarei45.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int M;
+    cout << "type your number :";
+    cin >> M;
+   for (int i=1;i<M; i++)
+   cout << i;
+
+    return 0;
+}
